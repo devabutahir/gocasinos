e60b629e16bacb2c901e7a5d24826eb794a8754b
devabutahir <devtahir242@gmail.com>
2024-11-28 16:20:06 +0600

    gocasino

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+"user strict";
+
+$(document).ready(function () {
+	//menu header bar
+	$(".header-bar").on("click", function (e) {
+		$(".main-menu, .header-bar").toggleClass("active");
+	});
+	$(".main-menu li a").on("click", function (e) {
+		$(".main-menu, .header-bar").removeClass("active")
+		var element = $(this).parent("li");
+		if (element.hasClass("open")) {
+			element.removeClass("open");
+			element.find("li").removeClass("open");
+			element.find("ul").slideUp(300, "swing");
+		} else {
+			element.addClass("open");
+			element.children("ul").slideDown(300, "swing");
+			element.siblings("li").children("ul").slideUp(300, "swing");
+			element.siblings("li").removeClass("open");
+			element.siblings("li").find("li").removeClass("open");
+			element.siblings("li").find("ul").slideUp(300, "swing");
+		}
+	});
+	//menu header bar
+	
+
+	//menu top fixed bar
+	var fixed_top = $(".header-section");
+	$(window).on("scroll", function () {
+		if ($(this).scrollTop() > 220) {
+			fixed_top.addClass("menu-fixed animated fadeInDown");
+			fixed_top.removeClass("slideInUp");
+			$("body").addClass("body-padding");
+		} else {
+			fixed_top.removeClass("menu-fixed fadeInDown");
+			fixed_top.addClass("slideInUp");
+			$("body").removeClass("body-padding");
+		}
+	});
+	//menu top fixed bar
+
+
+
+
+
+	// wow animation
+	new WOW().init();
+	// wow animation
+
+	//preloader
+	setTimeout(function(){
+		$('.bg-load').fadeToggle();
+	}, 1500);
+	//preloader
+	
+});
+
+
+
